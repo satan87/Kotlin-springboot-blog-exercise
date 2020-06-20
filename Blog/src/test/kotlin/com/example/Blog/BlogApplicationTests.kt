1261e6fa34dda35e81b42c56d5e58306c3f78e90
nicolas <nicolas.savoini@mac.com>
2020-06-20 13:06:48 -0400

    First Integration test

--- Blog/src/test/kotlin/com/example/Blog/BlogApplicationTests.kt
+++ Blog/src/test/kotlin/com/example/Blog/BlogApplicationTests.kt
@@ -1,13 +1,22 @@
 package com.example.Blog
 
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
 @SpringBootTest
-class BlogApplicationTests {
+class BlogApplicationTests() {
 
 	@Test
 	fun contextLoads() {
+		
 	}
+	
 
 }
